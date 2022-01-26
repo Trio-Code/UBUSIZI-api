3872334665b585be5ea10a4199771c1ae14534b5
Tresormugisha <ahirwemugishat53@gmail.com>
2022-01-26 16:28:22 +0200

    Added some extensions on video

--- src/middlewares/validation/post.js
+++ src/middlewares/validation/post.js
@@ -12,7 +12,7 @@ export const addPost = (req, res, next) => {
     validator(addTextSchema, req.body, res, next);
   } else {
     const ext = extension(req.files);
-    if ((type === 'Audio' && (ext === 'mp3' || ext === 'm4a')) || (type === 'Image' && (ext === 'jpg' || ext === 'JPG' || ext === 'png' || ext === 'jpeg' || ext === 'JPEG' || ext === 'pdf')) || (type === 'Video' && (ext === 'mp4' || ext === 'MP4' || ext === 'vob' || ext === '3gp'))) {
+    if ((type === 'Audio' && (ext === 'mp3' || ext === 'm4a')) || (type === 'Image' && (ext === 'jpg' || ext === 'JPG' || ext === 'png' || ext === 'jpeg' || ext === 'JPEG' || ext === 'pdf')) || (type === 'Video' && (ext === 'mp4' || ext === 'MP4' || ext === 'vob' || ext === '3gp' || ext === 'MKV' || ext === 'GIF'))) {
       validator(addFileSchema, req.body, res, next);
     } else {
       return out(res, 400, 'Upload a file that matches the post type', null, 'BAD_REQUEST');
